bbeb7aee4077352212aa1844f8c22d94fd67d703
Jeffrey <jeffreylee37@gmail.com>
2014-01-30 17:03:44 -0600

    Added GamepadDriveRotationCommand and GamepadDriveStrafeCommand.
    Added GamepadDriveRotationCommand and GamepadDriveStrafeCommand, which
    control the drivetrain via gamepad.

---
+++ src/com/edinarobotics/zeke/commands/GamepadDriveStrafeCommand.java
@@ -0,0 +1,44 @@
+
+package com.edinarobotics.zeke.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import com.edinarobotics.utils.gamepad.Gamepad;
+import com.edinarobotics.zeke.subsystems.DrivetrainStrafe;
+import com.edinarobotics.zeke.Components;
+import com.edinarobotics.utils.gamepad.GamepadAxisState;
+
+public class GamepadDriveStrafeCommand extends Command {
+    private Gamepad gamepad;
+    private GamepadAxisState gamepadAxisState;
+    private DrivetrainStrafe drivetrainStrafe;
+    
+    public GamepadDriveStrafeCommand(Gamepad gamepad) {
+        super("GamepadDriveStrafe");
+        this.gamepad = gamepad;
+        this.drivetrainStrafe = Components.getInstance().drivetrainStrafe;
+        requires(drivetrainStrafe);
+    }
+
+    protected void initialize() {
+        drivetrainStrafe.setMecanumPolarStrafe(0, 0);
+    }
+
+    protected void execute() {
+        double magnitude = gamepad.getGamepadAxisState().getLeftMagnitude();
+        double direction = gamepad.getGamepadAxisState().getLeftDirection();
+        drivetrainStrafe.setMecanumPolarStrafe(magnitude, direction);
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+        drivetrainStrafe.setMecanumPolarStrafe(0, 0);
+    }
+
+    protected void interrupted() {
+        end();
+    }
+    
+}
