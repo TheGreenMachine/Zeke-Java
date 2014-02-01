61ec48ef3a42a0b13143fd251fec7fbb7b3003be
Eric <congpeng89@gmail.com>
2014-01-31 20:41:17 -0600

    Added SetShooterCommand Class.
    SetShooterCommand sets the winch's state.

---
+++ src/com/edinarobotics/zeke/commands/SetShooterCommand.java
@@ -0,0 +1,42 @@
+package com.edinarobotics.zeke.commands;
+
+import com.edinarobotics.zeke.Components;
+import com.edinarobotics.zeke.subsystems.Shooter;
+import com.edinarobotics.zeke.subsystems.Shooter.WinchState;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class SetShooterCommand extends Command {
+
+    private Shooter shooter;
+    private DoubleSolenoid.Value value;
+    private WinchState winchState;
+    
+    public SetShooterCommand(WinchState winchState) {
+        super("SetShooter");
+        this.shooter = Components.getInstance().shooter;
+        this.winchState = winchState;
+        requires(shooter);
+    }
+    
+    protected void initialize() {
+        shooter.setWinchState(winchState);
+    }
+
+    protected void execute() {
+        
+    }
+
+    protected boolean isFinished() {
+        return true;
+    }
+
+    protected void end() {
+        
+    }
+
+    protected void interrupted() {
+        
+    }
+    
+}
