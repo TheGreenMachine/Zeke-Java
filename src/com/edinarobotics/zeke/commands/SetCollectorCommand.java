3c032b026d36606d611f23b7bd9fc21e46d6f84a
Srujun Gupta <theununtrium@gmail.com>
2014-02-18 19:53:33 -0600

    Rewrote SetCollectorCommand to use the new CollectorState enum.
    There are now 3 constructors that can be used according to what controls
    are needed.

--- src/com/edinarobotics/zeke/commands/SetCollectorCommand.java
+++ src/com/edinarobotics/zeke/commands/SetCollectorCommand.java
@@ -1,44 +1,55 @@
-
 package com.edinarobotics.zeke.commands;
 
 import com.edinarobotics.zeke.Components;
 import com.edinarobotics.zeke.subsystems.Collector;
+import com.edinarobotics.zeke.subsystems.Collector.CollectorState;
 import com.edinarobotics.zeke.subsystems.Collector.CollectorWheelState;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class SetCollectorCommand extends Command {
     
     private Collector collector;
-    private boolean isDeployed;
+    private CollectorState collectorState;
     private CollectorWheelState collectorWheelState;
     
-    public SetCollectorCommand(boolean isDeployed, CollectorWheelState collectorWheelState) {
+    {
         this.collector = Components.getInstance().collector;
-        this.isDeployed = isDeployed;
+        requires(this.collector);
+    }
+    
+    public SetCollectorCommand(CollectorState collectorState, CollectorWheelState collectorWheelState) {
+        this.collectorState = collectorState;
         this.collectorWheelState = collectorWheelState;
-        requires(collector);
+    }
+    
+    public SetCollectorCommand(CollectorState collectorState) {
+        this.collectorState = collectorState;
+    }
+    
+    public SetCollectorCommand(CollectorWheelState collectorWheelState) {
+        this.collectorWheelState = collectorWheelState;
     }
     
     protected void initialize() {
-       collector.setDeployed(isDeployed);
-       collector.setCollectorWheelState(collectorWheelState);
+        if(collectorState != null) {
+            collector.setDeployed(collectorState);
+        }
+        if(collectorWheelState != null) {
+            collector.setWheelState(collectorWheelState);
+        }
     }
 
     protected void execute() {
-        
     }
 
     protected boolean isFinished() {
         return true;
-        
     }
 
     protected void end() {
-        
     }
 
     protected void interrupted() {
-        
     }
     
 }
