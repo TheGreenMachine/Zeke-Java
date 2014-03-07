6e8e7993102addfb673e6d2ecf53ff2522185eb3
Jeffrey Lee <jeffreylee37@gmail.com>
2014-03-06 22:29:34 -0600

    Add PID systems to DriveXDistanceCommand.
    DriveXDistanceCommand can be configured to control its rotation while
    driving forward to stay in a straight line.

--- src/com/edinarobotics/zeke/commands/DriveXDistanceCommand.java
+++ src/com/edinarobotics/zeke/commands/DriveXDistanceCommand.java
@@ -1,9 +1,12 @@
 package com.edinarobotics.zeke.commands;
 
+import com.edinarobotics.utils.pid.PIDConfig;
+import com.edinarobotics.utils.pid.PIDTuningManager;
 import com.edinarobotics.zeke.Components;
 import com.edinarobotics.zeke.subsystems.Drivetrain;
 import com.edinarobotics.zeke.subsystems.DrivetrainRotation;
 import com.edinarobotics.zeke.subsystems.DrivetrainStrafe;
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveXDistanceCommand extends Command {
@@ -11,34 +14,54 @@ public class DriveXDistanceCommand extends Command {
     private DrivetrainStrafe drivetrainStrafe;
     private DrivetrainRotation drivetrainRotation;
     private double distanceThreshold;
+    private double speed;
+    private boolean isForward;
+    private final double P = 0.5, I = 0.0, D = 0.0, F = 0.0;
+    PIDConfig pidConfig;
+    PIDController pidController;
     
-    private final double FORWARD_SPEED = 0.5;
-    
-    public DriveXDistanceCommand(double distanceThreshold) {
+    public DriveXDistanceCommand(double distanceThreshold, double speed, boolean isForward) {
         super("DriveXDistanceCommand");
         this.drivetrain = Components.getInstance().drivetrain;
         this.drivetrainStrafe = Components.getInstance().drivetrainStrafe;
         this.drivetrainRotation = Components.getInstance().drivetrainRotation;
         this.distanceThreshold = distanceThreshold;
+        this.speed = speed;
+        this.isForward = isForward;
+        pidConfig = PIDTuningManager.getInstance().getPIDConfig("DriveXDistance");
+        pidController = new PIDController(P, I, D, F, drivetrainRotation.getGyro(), drivetrainRotation, 0.025);
+        pidController.setAbsoluteTolerance(5.0);
         requires(drivetrainStrafe);
         requires(drivetrainRotation);
     }
     
     protected void initialize() {
-        drivetrainStrafe.setMecanumPolarStrafe(FORWARD_SPEED, 0.0);
+        drivetrainStrafe.setMecanumPolarStrafe(speed, isForward ? 90.0 : 270.0);
         drivetrainRotation.setMecanumPolarRotate(0.0);
+        drivetrainRotation.resetGyro();
+        pidController.reset();
+        pidController.enable();
     }
 
     protected void execute() {
-        drivetrainStrafe.setMecanumPolarStrafe(FORWARD_SPEED, 0.0);
-        drivetrainRotation.setMecanumPolarRotate(0.0);
+        drivetrainStrafe.setMecanumPolarStrafe(speed, isForward ? 90.0 : 270.0);
+        pidConfig.setSetpoint(0.0);
+        pidConfig.setValue(drivetrainRotation.getGyroAngle());
+        pidController.setPID(pidConfig.getP(P), pidConfig.getI(I), pidConfig.getD(D), pidConfig.getF(F));
+        pidController.setSetpoint(pidConfig.getSetpoint());
+        System.out.println(drivetrainRotation.getGyroAngle() + "  " + drivetrainRotation.getRotation());
     }
 
     protected boolean isFinished() {
-        return drivetrain.getUltrasonicSensor().getDistance() < distanceThreshold;
+        if(isForward) {
+            return drivetrain.getUltrasonicSensor().getDistance() < distanceThreshold;
+        } else {
+            return drivetrain.getUltrasonicSensor().getDistance() > distanceThreshold;
+        }
     }
 
     protected void end() {
+        pidController.disable();
         drivetrainStrafe.setMecanumPolarStrafe(0, 0);
         drivetrainRotation.setMecanumPolarRotate(0);
     }
