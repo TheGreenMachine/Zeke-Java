bdc06a46ce9f5b4aab518c5e5f6caf1459d88c18
Jeffrey Lee <jeffreylee37@gmail.com>
2014-04-09 20:48:08 -0500

    Removed InternalSetCollectorCommand to change command flow.
    Added InternalValveCommand which handles the collector valve.
    The command will no longer be improperly interrupted.

--- src/com/edinarobotics/zeke/commands/SetCollectorCommand.java
+++ src/com/edinarobotics/zeke/commands/SetCollectorCommand.java
@@ -7,21 +7,7 @@ import com.edinarobotics.zeke.subsystems.Collector.CollectorWheelState;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.StartCommand;
 
-public class SetCollectorCommand extends StartCommand {
-    
-    public SetCollectorCommand(CollectorState collectorState, CollectorWheelState collectorWheelState){
-        super(new InternalSetCollectorCommand(collectorState, collectorWheelState));
-    }
-    
-    public SetCollectorCommand(CollectorState collectorState){
-        super(new InternalSetCollectorCommand(collectorState));
-    }
-    
-    public SetCollectorCommand(CollectorWheelState collectorWheelState){
-        super(new InternalSetCollectorCommand(collectorWheelState));
-    }
-    
-    private static class InternalSetCollectorCommand extends Command {
+public class SetCollectorCommand extends Command {
     
         private Collector collector;
         private CollectorState collectorState;
@@ -31,25 +17,29 @@ public class SetCollectorCommand extends StartCommand {
             this.collector = Components.getInstance().collector;
             requires(this.collector);
             setInterruptible(true);
-            setTimeout(1.0);
         }
         
-        private InternalSetCollectorCommand(CollectorState collectorState, CollectorWheelState collectorWheelState) {
+        public SetCollectorCommand(CollectorState collectorState, CollectorWheelState collectorWheelState) {
             this.collectorState = collectorState;
             this.collectorWheelState = collectorWheelState;
         }
         
-        public InternalSetCollectorCommand(CollectorState collectorState) {
+        public SetCollectorCommand(CollectorState collectorState) {
             this.collectorState = collectorState;
         }
         
-        public InternalSetCollectorCommand(CollectorWheelState collectorWheelState) {
+        public SetCollectorCommand(CollectorWheelState collectorWheelState) {
             this.collectorWheelState = collectorWheelState;
         }
         
         protected void initialize() {
             if(collectorState != null) {
-                collector.setDeployed(collectorState);
+                if(!(collectorState.equals(CollectorState.DEPLOY) && collector.getState().equals(CollectorState.VALVE_VENT))){
+                    collector.setDeployed(collectorState);
+                    if(collectorState.equals(CollectorState.DEPLOY)){
+                        new InternalValveCommand(collector).start();
+                    }
+                }
             }
             if(collectorWheelState != null) {
                 collector.setWheelState(collectorWheelState);
@@ -57,28 +47,50 @@ public class SetCollectorCommand extends StartCommand {
         }
         
         protected void execute() {
-            if(collectorState != null) {
-                collector.setDeployed(collectorState);
-            }
-            if(collectorWheelState != null) {
-                collector.setWheelState(collectorWheelState);
-            }
         }
         
         protected boolean isFinished() {
-            if(collectorState != null){
-                return !collectorState.equals(CollectorState.DEPLOY) || isTimedOut();
-            }
             return true;
         }
         
         protected void end() {
-            if(collectorState != null && collectorState.equals(CollectorState.DEPLOY)){
-                collector.setDeployed(CollectorState.VALVE_VENT);
-            }
         }
         
         protected void interrupted() {
         }
+    
+    private static class InternalValveCommand extends Command {
+
+        private Collector collector;
+        
+        public InternalValveCommand(Collector collector) {
+            this.collector = collector;
+            setTimeout(1.0);
+        }
+        
+        protected void initialize() {
+        }
+
+        protected void execute() {
+        }
+
+        protected boolean isFinished() {
+            CollectorState collectorState = collector.getState();
+            if(collectorState != null) {
+                return !collectorState.equals(CollectorState.DEPLOY) || isTimedOut();
+            }
+            return true;
+        }
+
+        protected void end() {
+            CollectorState collectorState = collector.getState();
+            if(collectorState != null && collectorState.equals(CollectorState.DEPLOY)) {
+                collector.setDeployed(CollectorState.VALVE_VENT);
+            }
+        }
+
+        protected void interrupted() {
+        }
+        
     }
 }
