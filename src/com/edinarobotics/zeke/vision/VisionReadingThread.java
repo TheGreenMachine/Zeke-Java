4aec2e09c903ff96bedfa895ff7a6837f769a8df
Karl Otness <karl@otness.com>
2014-05-07 19:45:17 -0500

    Add VisionServer and framework to the robot.

---
+++ src/com/edinarobotics/zeke/vision/VisionReadingThread.java
@@ -0,0 +1,68 @@
+package com.edinarobotics.zeke.vision;
+
+import com.edinarobotics.utils.log.Level;
+import edu.wpi.first.wpilibj.Timer;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.microedition.io.SocketConnection;
+
+public class VisionReadingThread extends Thread {
+
+    private volatile boolean stop;
+    private SocketConnection connection;
+    private VisionServer server;
+    private static final double TIMEOUT = 10.0;
+
+    public VisionReadingThread(SocketConnection connection, VisionServer server) {
+        stop = false;
+        this.connection = connection;
+        this.server = server;
+    }
+
+    public void requestStop() {
+        this.stop = true;
+    }
+
+    public void run() {
+        InputStream inputStream = null;
+        try {
+            inputStream = connection.openInputStream();
+            byte[] readBytes = new byte[5];
+            double lastHeartbeat = Timer.getFPGATimestamp();
+            while (!stop) {
+                try {
+                    if (Timer.getFPGATimestamp() < lastHeartbeat + TIMEOUT) {
+                        requestStop();
+                        break;
+                    }
+                    int inputData = inputStream.read(readBytes);
+                    for (int i = 0; i < inputData; i++) {
+                        byte reading = readBytes[i];
+                        boolean status = reading != 0;
+                        System.out.println(">>>"+(status?"HOT HOT HOT":"COLD COLD COLD"));
+                        this.server.reportHotGoal(status);
+                    }
+                    lastHeartbeat = Timer.getFPGATimestamp();
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            try {
+                this.connection.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/com/edinarobotics/zeke/vision/VisionServer.java
@@ -0,0 +1,83 @@
+package com.edinarobotics.zeke.vision;
+
+import com.edinarobotics.utils.log.LogSystem;
+import com.edinarobotics.utils.log.Logger;
+
+public class VisionServer {
+
+    private static VisionServer instance;
+    private int port;
+    private Logger logger;
+    private VisionConnectThread connThread;
+    private int counts;
+    private boolean requireSequential;
+
+    private VisionServer() {
+        this.port = 1180;
+        this.logger = LogSystem.getLogger("zeke.vision");
+        counts = 0;
+        requireSequential = false;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+        start();
+    }
+
+    public static VisionServer getInstance() {
+        if (instance == null) {
+            instance = new VisionServer();
+        }
+        return instance;
+    }
+    
+    public void setRequireSequential(boolean requireSequential){
+        this.requireSequential = requireSequential;
+    }
+
+    public void start() {
+        stop();
+        connThread = new VisionConnectThread(this.port, this);
+    }
+
+    public void stop() {
+        if (connThread != null && connThread.isAlive()) {
+            connThread.requestStop();
+            try {
+                connThread.join();
+                VisionReadingThread readThread = connThread.getReadingThread();
+                if (readThread != null && readThread.isAlive()) {
+                    readThread.requestStop();
+                    readThread.join();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void resetCounts() {
+        counts = 0;
+    }
+
+    public VisionConnectThread getConnectThread() {
+        return connThread;
+    }
+
+    public void incrementCount() {
+        counts++;
+    }
+
+    public void reportHotGoal(boolean isHot) {
+        if(requireSequential && !isHot){
+            resetCounts();
+        }
+        if (isHot) {
+            incrementCount();
+        }
+    }
+
+    public int getCount() {
+        return counts;
+    }
+}
