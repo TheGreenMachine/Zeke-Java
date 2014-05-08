f304f218aa74e6a5f7263777571e59be29541192
Karl Otness <karl@otness.com>
2014-05-08 15:40:25 -0500

    Change VisionServer to start the connection thread.

--- src/com/edinarobotics/zeke/vision/VisionServer.java
+++ src/com/edinarobotics/zeke/vision/VisionServer.java
@@ -38,6 +38,7 @@ public class VisionServer {
     public void start() {
         stop();
         connThread = new VisionConnectThread(this.port, this);
+        connThread.start();
     }
 
     public void stop() {
