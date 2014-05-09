16b0745aac123dea441ee0acb7ff7184261deee3
Karl Otness <karl@otness.com>
2014-05-08 20:01:04 -0500

    Change two ball autonomous to drive forward while collection.
    This will help the robot capture the ball even if it rolls slightly
    forward while collecting.

--- src/com/edinarobotics/zeke/commands/AutonomousCommand.java
+++ src/com/edinarobotics/zeke/commands/AutonomousCommand.java
@@ -7,8 +7,14 @@ public class AutonomousCommand extends CommandGroup {
 
     public AutonomousCommand(boolean twoBallAuto) {
         if (twoBallAuto) {
-            this.addSequential(new ShootingSequenceCommand(true));
-            this.addSequential(new RunCollectorCommand(2.0, true));
+            CommandGroup driveSubsequence = new CommandGroup();
+            driveSubsequence.addSequential(new WaitCommand(0.75));
+            driveSubsequence.addSequential(new DriveXTimeCommand(0.5, 0.35));
+            
+            this.addSequential(new ShootingSequenceCommand(false));
+            this.addParallel(new LowerShooterAfterWaitCommand(1.5));
+            this.addParallel(driveSubsequence);
+            this.addSequential(new RunCollectorCommand(3.5, true));
             this.addSequential(new ShootingSequenceCommand(false));
             this.addParallel(new LowerShooterAfterWaitCommand(1.5));
             this.addSequential(new WaitCommand(0.5));
