00b703a94d7e3de1f4197c02f54fe27bcfe1cecf
Srujun Gupta <theununtrium@gmail.com>
2014-05-10 20:13:43 -0500

    Remove unnecessary RunCollectorCommand and fix Autonomous sequence.
    Autonomous now does the drive forward while collecting sequence only after
    the shooter has lowered completely.

--- src/com/edinarobotics/zeke/commands/AutonomousCommand.java
+++ src/com/edinarobotics/zeke/commands/AutonomousCommand.java
@@ -1,5 +1,6 @@
 package com.edinarobotics.zeke.commands;
 
+import com.edinarobotics.zeke.subsystems.Collector;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
@@ -7,18 +8,29 @@ public class AutonomousCommand extends CommandGroup {
 
     public AutonomousCommand(boolean twoBallAuto) {
         if (twoBallAuto) {
-            CommandGroup driveSubsequence = new CommandGroup();
-            driveSubsequence.addSequential(new WaitCommand(0.75));
-            driveSubsequence.addSequential(new DriveXTimeCommand(0.5, 0.35));
+            CommandGroup lowerAndDeploy = new CommandGroup();
+            lowerAndDeploy.addParallel(new LowerShooterCommand());
+            lowerAndDeploy.addParallel(new SetCollectorCommand(Collector.CollectorState.DEPLOY));
+            
+            CommandGroup driveAndCollect = new CommandGroup();
+            driveAndCollect.addParallel(new SetCollectorCommand(Collector.CollectorWheelState.COLLECTING));
+            driveAndCollect.addParallel(new DriveXTimeCommand(0.5, 0.35));
+            driveAndCollect.addSequential(new WaitCommand(1.5));
+            
+            CommandGroup retractWhileCollecting = new CommandGroup();
+            retractWhileCollecting.addParallel(new WaitCommand(2.0));
+            retractWhileCollecting.addParallel(new SetCollectorCommand(Collector.CollectorState.RETRACT,
+                    Collector.CollectorWheelState.COLLECTING));
             
             this.addSequential(new ShootingSequenceCommand(false));
-            this.addParallel(new LowerShooterAfterWaitCommand(1.5));
-            this.addParallel(driveSubsequence);
-            this.addSequential(new RunCollectorCommand(3.5, true));
+            this.addSequential(new WaitCommand(1.5));
+            this.addSequential(lowerAndDeploy);
+            this.addSequential(driveAndCollect);
+            this.addSequential(retractWhileCollecting);
             this.addSequential(new ShootingSequenceCommand(false));
-            this.addParallel(new LowerShooterAfterWaitCommand(1.5));
-            this.addSequential(new WaitCommand(0.5));
-            this.addSequential(new DriveXTimeCommand(3.5, 0.35));
+            this.addParallel(new DriveXTimeCommand(3.5, 0.35));
+            this.addSequential(new WaitCommand(1.5));
+            this.addSequential(new LowerShooterCommand());
         } else {
             this.addSequential(new ShootingSequenceCommand(false));
             this.addParallel(new LowerShooterAfterWaitCommand(1.5));
